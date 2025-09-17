d75ae077600b2c34212caf0e51bf1cc8b253c3e7
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-09-17 13:13:23 +0530

    Add files via upload

---
+++ ArrayNormal.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class ArrayNormal {
+
+	public static void main(String []args) {
+		int[]arr= {1,2,3,4,5};
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(" elements are : "+arr[i]);
+		}
+		int sum=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			sum+=arr[i];
+		}
+		
+		System.out.println(sum);
+	}
+}
