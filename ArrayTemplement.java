43ab0a25765c90c9e439b046941e23a2ed21d2fa
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-09-29 22:14:10 +0530

    Add files via upload

---
+++ ArrayTemplement.java
@@ -0,0 +1,28 @@
+package NewPack;
+import java.util.Scanner;
+public class ArrayTemplement {
+	 //type[] arrayName;
+	int[] a1;
+	//int[] arrayName=new int[s];
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the nuber of elemenets");
+int n=sc.nextInt();
+int[] arr=new int[n];//array initialize
+//input
+System.out.println("enter"+n+"elements");
+for(int i=0;i<n;i++)
+{
+	arr[i]=sc.nextInt();
+}
+//print or output
+for(int i=0;i<=n;i++)
+{
+	System.out.println("the array elements are");
+	System.out.println(arr[i]+"");
+}sc.close();
+	}
+
+}
