c5ace286034a00cf3aec0c5c98414fc19284d618
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-09-29 16:35:54 +0530

    Add files via upload

---
+++ ArrayListImpl1.java
@@ -0,0 +1,21 @@
+package pack;
+
+public class ArrayListImpl1 {
+public static void main(String []args)
+{
+	//int a[];
+	//arraylist Integer
+	ArrayList<Integer> a=new ArrayList<>();
+	a.add(100);
+	a.add(200);
+	a.add(400);
+	a.add(0,500);
+	System.out.println(a);
+	ArrayList<object> mix=new ArrayList<>();
+	mix.add(100);
+	mix.add("String");
+	mix.add(0,9);
+	mix.add();
+	
+	}
+}
