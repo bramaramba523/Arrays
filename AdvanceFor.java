6fdc0c510c6b711a1d7f4028216799180956f5f6
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-09-29 16:56:42 +0530

    Add files via upload

---
+++ AdvanceFor.java
@@ -0,0 +1,15 @@
+package pack11;
+
+public class AdvanceFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//for each on advanced manner
+		int [] numbers= {1,2,3,4,5};//[]-represents array
+		for(int num:numbers)//passing array to num variable
+		{
+			System.out.println(num);
+		}
+	}
+
+}
