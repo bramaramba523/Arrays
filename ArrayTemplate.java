8e1d1c08b217dcd82c2cacce9515849b270856a4
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-09-29 16:50:46 +0530

    Add files via upload

---
+++ ArrayTemplate.java
@@ -0,0 +1,26 @@
+package javaPG;
+
+import java.util.Scanner;
+
+public class ArrayTemplate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the nuber of elemenets");
+int n=sc.nextInt();
+int[] arr=new int[n];//array initialize
+//input
+System.out.println("enter"+n+"elements");
+for(int i=0;i<n;i++)
+{
+	arr[i]=sc.nextInt();
+}
+//print or output
+for(int i=0;i<=n;i++)
+{
+	System.out.println("the array elements are");
+	System.out.println(arr[i]+"");
+}sc.close();
+	}
+}
